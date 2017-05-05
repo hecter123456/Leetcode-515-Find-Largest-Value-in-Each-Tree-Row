eb1f3783872d38084eec7f4fde847b66b647706e
hecter_wu <hecter_wu@transcend.com.tw>
2017-05-05 18:23:44 +0800

    add test case testThirdLevelTree

--- Leetcode_515_Find_Largest_Value_in_Each_Tree_Row.py
+++ Leetcode_515_Find_Largest_Value_in_Each_Tree_Row.py
@@ -10,6 +10,15 @@ class unitest(unittest.TestCase):
     def testNone(self):
         root = None
         self.assertEqual(Solution().largestValues(root),None);
+    def testThirdLevelTree(self):
+        root = TreeNode(2)
+        root.left = TreeNode(1)
+        root.right = TreeNode(3)
+        root.left.right = TreeNode(5)
+        root.right.left = TreeNode(4)
+        root.left.left = TreeNode(6)
+        root.right.right = TreeNode(7)
+        self.assertEqual(Solution().largestValues(root),[2,3,7]);
 
 class Solution(object):
     def largestValues(self, root):
