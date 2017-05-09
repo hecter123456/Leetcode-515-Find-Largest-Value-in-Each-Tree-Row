455da0a22f707636bdebf2666d07658304a1b953
hecter_wu <hecter_wu@transcend.com.tw>
2017-05-09 14:38:05 +0800

    refactor: import Treesolution to add Binary Tree Node

--- Leetcode_515_Find_Largest_Value_in_Each_Tree_Row.py
+++ Leetcode_515_Find_Largest_Value_in_Each_Tree_Row.py
@@ -1,54 +1,35 @@
 import unittest
-
-class TreeNode(object):
-    def __init__(self, x = None):
-        self.val = x
-        self.left = None
-        self.right = None
+import TreeSolution
 
 class unitest(unittest.TestCase):
     def testNone(self):
-        root = None
+        row = []
+        tree = TreeSolution.TreeSolution()
+        root = tree.AddBinaryTreeNode(row)
         self.assertEqual(Solution().largestValues(root),[]);
     def testThirdLevelTree(self):
-        row = [2,1,3,6,5,4,7]
-        root = TreeSolution().AddTreeNode(row)
-        self.assertEqual(Solution().largestValues(root),[2,3,7]);
+        row = [8,1,3,6,5,4,7]
+        tree = TreeSolution.TreeSolution()
+        root = tree.AddBinaryTreeNode(row)
+        self.assertEqual(Solution().largestValues(root),[8,3,7]);
     def testAnotherCase(self):
         row = [1,3,2,5,3,None,9]
-        root = TreeSolution().AddTreeNode(row)
+        tree = TreeSolution.TreeSolution()
+        root = tree.AddBinaryTreeNode(row)
         self.assertEqual(Solution().largestValues(root),[1,3,9]);
-class TreeSolution(object):
-    def AddTreeNode(self,row):
-        if not row:
-            return None
-        i = 0
-        root = TreeNode(row[i])
-        i = i+1
-        queue = [root]
-        for node in queue:
-            if i < len(row):
-                node.left = TreeNode(row[i])
-                i = i+1
-            if i < len(row):
-                node.right = TreeNode(row[i])
-                i = i+1
-            queue += filter(None, (node.left,node.right))
-        return root
+
 class Solution(object):
-    def maxFunc(self,a,b):
-        if a is None:
-            return b
-        elif b is None:
-            return a
-        else:
-            return max(a,b)
     def largestValues(self, root):
         if not root:
             return []
         left = self.largestValues(root.left)
         right = self.largestValues(root.right)
-        return [root.val] + list(map(self.maxFunc, left, right))  
+        if left == []:
+            return [root.val] + right
+        elif right == []:
+            return [root.val] + left
+        else:
+            return [root.val] + list(map(max,left, right))
 
 if __name__ == '__main__':
     unittest.main()
