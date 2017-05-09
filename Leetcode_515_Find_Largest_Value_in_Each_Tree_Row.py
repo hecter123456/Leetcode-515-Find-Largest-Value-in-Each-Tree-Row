a5a34b2fddecf9bb16d99666e8ba82139c069eb9
hecter_wu <hecter_wu@transcend.com.tw>
2017-05-09 11:04:19 +0800

    refactor: using Divide and conquer

--- Leetcode_515_Find_Largest_Value_in_Each_Tree_Row.py
+++ Leetcode_515_Find_Largest_Value_in_Each_Tree_Row.py
@@ -11,22 +11,44 @@ class unitest(unittest.TestCase):
         root = None
         self.assertEqual(Solution().largestValues(root),[]);
     def testThirdLevelTree(self):
-        root = TreeNode(2)
-        root.left = TreeNode(1)
-        root.right = TreeNode(3)
-        root.left.right = TreeNode(5)
-        root.right.left = TreeNode(4)
-        root.left.left = TreeNode(6)
-        root.right.right = TreeNode(7)
+        row = [2,1,3,6,5,4,7]
+        root = TreeSolution().AddTreeNode(row)
         self.assertEqual(Solution().largestValues(root),[2,3,7]);
+    def testAnotherCase(self):
+        row = [1,3,2,5,3,None,9]
+        root = TreeSolution().AddTreeNode(row)
+        self.assertEqual(Solution().largestValues(root),[1,3,9]);
+class TreeSolution(object):
+    def AddTreeNode(self,row):
+        if not row:
+            return None
+        i = 0
+        root = TreeNode(row[i])
+        i = i+1
+        queue = [root]
+        for node in queue:
+            if i < len(row):
+                node.left = TreeNode(row[i])
+                i = i+1
+            if i < len(row):
+                node.right = TreeNode(row[i])
+                i = i+1
+            queue += filter(None, (node.left,node.right))
+        return root
 class Solution(object):
+    def maxFunc(self,a,b):
+        if a is None:
+            return b
+        elif b is None:
+            return a
+        else:
+            return max(a,b)
     def largestValues(self, root):
-        ans = []
-        q = [root]
-        while any(q):
-            ans.append(max(node.val for node in q))
-            q = [kidnode for node in q for kidnode in (node.left, node.right) if kidnode]
-        return ans
+        if not root:
+            return []
+        left = self.largestValues(root.left)
+        right = self.largestValues(root.right)
+        return [root.val] + list(map(self.maxFunc, left, right))  
 
 if __name__ == '__main__':
     unittest.main()
